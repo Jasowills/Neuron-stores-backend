2196e07294493ddd8dd8fdcece351707ee44dff8
Jasowills <Jujuzhaddy01@gmail.com>
2023-07-05 13:42:43 +0100

    origin

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './src/modules/app.module';
+import { CorsOptions } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure CORS options
+  const corsOptions: CorsOptions = {
+    origin: '*', // Set your desired origin or multiple origins here
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
+
 bootstrap();
